6fddc038b55ac250edbdbb2d519950b18f5d2196
jsmith <jasmith79@gmail.com>
2018-01-10 13:40:53 -0500

    Added console output

--- src/utils/server.js
+++ src/utils/server.js
@@ -38,7 +38,10 @@ const toObjectString = (o, n=0) => {
 app.get('*', function(request, response, next) {
   console.log(`URL: ${request.url}`);
   if (request.url.match(/\.[0-9a-zA-Z]*$/)) {
+    console.log(`File ${request.url} requested, serving...`);
     response.sendFile(`${workingDirectory}/${request.url}`);
+    console.log('done.');
+    return;
   } else {
     next();
   }
@@ -46,13 +49,20 @@ app.get('*', function(request, response, next) {
   let index = request.url.match(/\/$/) ? request.url : `${request.url}/`;
   let pathPieces = request.url.split('/').filter(x => x);
   try {
+    console.log('Checking directory for index.html...');
     const index = request.url.match(/\/$/) ? request.url : `${request.url}/`;
     response.sendFile(`${workingDirectory}/${index}index.html`);
+    console.log('done.');
+    return;
   } catch (e) {
+    console.log('not found.');
     next();
   }
 }, function(request, response) {
+  console.log('Serving local index.html...');
   response.sendFile(`${workingDirectory}/index.html`);
+  console.log('done.');
+  return;
 });
 
 app.listen(8080, _ => console.log(`ui-components dev server listening at localhost:8080.`));
