678172fb1a47a168e252604a3060c27773e27dce
jsmith <jasmith79@gmail.com>
2018-01-10 13:37:48 -0500

    Updated test page and js

--- test/adhoc/index.js
+++ test/adhoc/index.js
@@ -7,6 +7,7 @@ import '../../src/elements/tabs.js';
 import '../../src/elements/text.js';
 import '../../src/elements/input.js';
 import '../../src/elements/drop-down.js';
+import '../../src/elements/login.js';
 import { Router } from '../../src/elements/router.js';
 
 // import Styled from '../../src/utils/styler.js';
