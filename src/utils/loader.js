61b61bea77b820443f1cb266fcc052108ee3e733
jsmith <jasmith79@gmail.com>
2018-01-20 12:18:25 -0500

    Reworked loader

--- src/utils/loader.js
+++ src/utils/loader.js
@@ -1,14 +1,21 @@
 /*
+ * loader.js
+ * @author jarsmith79@gmail.com
+ * @copyright 2018
+ * @license MIT
  *
- *
- * This uses `document.write`. While there are plenty of good reasons to avoid it, the WebComponents
- * polyfills *must* be loaded synchronously. Rather than try to suss out via polling for the other
- * polyfills I've loaded them synchronously as well.
- *
- * Obviously be sure if you include this that it's before the end of the document.
+ * Loads all of the basic ui-components and associated libraries and polyfills.
  */
 
 ;(function(global) {
+  var makeScript = function(src, module_, async_) {
+    var script = global.document.createElement('script');
+    script.src = src;
+    script.setAttribute('defer', true);
+    if (module_) script.setAttribute('type', 'module');
+    if (async_) script.setAttribute('async', true);
+    return script;
+  };
 
   // First, test for IE < 11 and warn user
   var ie = global.navigator.userAgent.indexOf('MSIE ')
@@ -23,29 +30,19 @@
     // Figure out if we're somewhere other than the root
     var match = global.document.currentScript.outerHTML.match(/['"]{1}([\.\w\/\-]+\/)build\/loader/);
     var prefix = match ? match[1] : './';
-    console.log(prefix);
-    console.log(global.document.currentScript.outerHTML);
 
     var transpiled = prefix + 'build/es5.js';
-    var corejs = '<script src="' +
-      prefix +
-      'node_modules/babel-polyfill/dist/polyfill.min.js"></script>';
+    var corejs = prefix + 'node_modules/babel-polyfill/dist/polyfill.min.js';
+    var formdata = prefix + 'node_modules/formdata-polyfill/formdata.min.js';
 
-    var formdata = '<script src="' +
-      prefix +
-      'node_modules/formdata-polyfill/formdata.min.js"></script>';
+    var webcomplite = prefix +
+      'node_modules/@webcomponents/webcomponentsjs/webcomponents-lite.js';
 
-    var webcomplite = '<script src="' +
-      prefix +
-      'node_modules/@webcomponents/webcomponentsjs/webcomponents-lite.js"></script>';
+    var es5shim = prefix +
+      'node_modules/@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js';
 
-    var es5shim = '<script src="' +
-      prefix +
-      'node_modules/@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js"></script>';
-
-    var webcompmin = '<script src="' +
-      prefix +
-      'node_modules/@webcomponents/webcomponentsjs/webcomponents-sd-ce.js"></script>';
+    var webcompmin = prefix +
+      'node_modules/@webcomponents/webcomponentsjs/webcomponents-sd-ce.js';
 
     var needsTranspiled = false;
     var needsCoreJs = false;
@@ -98,7 +95,7 @@
       needsFormData = true;
     }
 
-    var module = !needsTranspiled && !needsCoreJs;
+    var module_ = !needsTranspiled && !needsCoreJs;
     var polyfills = [];
     if (needsCoreJs) {
       polyfills = [corejs, es5shim, webcomplite];
@@ -120,102 +117,24 @@
 
     var i = 0, poly = '';
     for (i; i < polyfills.length; ++i) {
-      poly += polyfills[i];
+      // poly += polyfills[i];
+      document.head.appendChild(makeScript(polyfills[i]));
     }
 
-    if (poly) {
-      document.write(poly);
-    } else {
-      console.log('No polyfills needed.');
-    }
+    if (!polyfills.length) console.log('No polyfills needed for ui-components-lite.');
 
-    if (module) {
-      var modulescript = global.document.createElement('script');
-      modulescript.type = 'module';
-      modulescript.src = prefix + 'build/index.js';
-      modulescript.setAttribute('async', true);
-      document.head.appendChild(modulescript);
+    if (module_) {
+      document.head.appendChild(makeScript(prefix + 'build/index.js', true, true));
     }
 
     if (!safari10point1) {
-      var nomodule = global.document.createElement('script');
+      var nomodule = makeScript(
+        prefix + (needsTranspiled ? 'build/es5.js' : 'build/concat.js'),
+        false,
+        true
+      );
       nomodule.setAttribute('nomodule', true);
-      nomodule.setAttribute('defer', true);
-      nomodule.setAttribute('async', true);
-      nomodule.src = prefix + (needsTranspiled ? 'build/es5.js' : 'build/concat.js');
       document.head.appendChild(nomodule);
     }
   }
 })(new Function('return this;')());
-
-// ;(function (global) {
-//   // First, test for IE < 11 and warn user
-//   var ie = global.navigator.userAgent.indexOf('MSIE ')
-//   if (ie > 0) {
-//     global.alert('You seem to be using Internet Explorer version ' +
-//       parseInt(ua.substring(ie + 5, ua.indexOf('.', ie)), 10) +
-//       '. Internet Explorer older than 11 is not supported by this' +
-//       ' site. Please upgrade to a modern browser like Microsoft Edge, ' +
-//       'Apples Safari, or Google Chrome.');
-//   } else {
-//     // Figure out if we're somewhere other than the root
-//     var match = global.document.currentScript.outerHTML.match(/['"]{1}(\.+[\.\/]+)/);
-//     var prefix = match ? match[1] : './';
-//
-//     // Test for and include WebComponents polyfills.
-//     var document = global.document;
-//     if (!('HTMLElement' in global &&
-//           'attachShadow' in global.HTMLElement.prototype &&
-//           'customElements' in global)
-//     ) {
-//       console.log('No webcomponents support. Loading polyfill...');
-//       document.write(
-//         '<script src="' +
-//         prefix +
-//         'node_modules/@webcomponents/webcomponentsjs/custom-elements-es5-adapter.js"></script>'
-//       );
-//
-//       document.write(
-//         '<script src="' +
-//         prefix +
-//         'node_modules/@webcomponents/webcomponentsjs/webcomponents-lite.js"></script>'
-//       );
-//
-//       global._usingShady = true;
-//       var globalStyleFixes = global.document.createElement('style');
-//       globalStyleFixes.innerHTML = '' +
-//         'ui-tab ui-checkbox { display: none; } ' +
-//         'ui-login ui-fab .content-wrapper .arrow { left: 27px; }';
-//
-//       document.head.appendChild(globalStyleFixes);
-//       // document.write(
-//       //   '<script src="' +
-//       //   prefix +
-//       //   'node_modules/@tuespetre/shadow-dom/dist/shadow-dom.min.js"></script>'
-//       // );
-//
-//       var js = document.createElement('script');
-//       js.setAttribute('async', true);
-//       js.setAttribute('defer', true);
-//       js.src = prefix + 'build/backup.js';
-//       global.document.head.appendChild(js);
-//     } else {
-//       console.log('Webcomponents supported. No polyfill needed');
-//     }
-//
-//     // Need this for pretty much all older browsers.
-//     // TODO: es6-collections is unmaintained. Need to replace this with a polyfill for WeakMap only.
-//     // unfortunately a lot of them seem to be CJS only. Maybe just go core-js?
-//     if (!('WeakMap' in global)) {
-//       console.log('Loading WeakMap polyfill...');
-//       document.write('<script src="../../node_modules/es6-collections/es6-collections.js"></script>');
-//     }
-//
-//     // Need the following for Safari which supports everything else. Elements that use this (e.g.
-//     // ui-form) should also import to be safe if using newer Safari.
-//     if (!('entries' in global.FormData.prototype)) {
-//       console.log('Loading FormData methods polyfill...');
-//       document.write('<script src="../../node_modules/formdata-polyfill/formdata.min.js"></script>');
-//     }
-//   }
-// })(new Function('return this;')());
